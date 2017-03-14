15d6a11e11ddb1a597ee5a2abd9ca6f6742fe802
VoNZproject <voiceofnewzealand@gmail.com>
2017-03-15 09:02:55 +1300

    editing a writing mistake

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -1,7 +1,7 @@
 <ul class="with-icon">
 	<li><a href="#" class="icon-file svg">General data </a></li>
 
-	<li><a href="#" class="icon-file svg">English profil</a></li>
+	<li><a href="#" class="icon-file svg">English profile</a></li>
 
-	<li><a href="#" class = "icon-file svg">Maori profil</a></li>
+	<li><a href="#" class = "icon-file svg">Maori profile</a></li>
 </ul>
